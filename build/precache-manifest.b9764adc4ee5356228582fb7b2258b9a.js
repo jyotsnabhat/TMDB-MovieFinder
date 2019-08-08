ec877bbc238f0ac85a57d2bd6e7a881617ca309e
jo@inthecode.com.au <coolgirl26>
2019-08-08 12:29:26 +1000

    production build

---
+++ build/precache-manifest.b9764adc4ee5356228582fb7b2258b9a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f23fafaba7ea15eabea60df3d46a5ce5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3097263639fcddd9bf7b",
+    "url": "/static/css/main.fcd46895.chunk.css"
+  },
+  {
+    "revision": "82edba6798e375b0d22a",
+    "url": "/static/js/2.f944cb70.chunk.js"
+  },
+  {
+    "revision": "3097263639fcddd9bf7b",
+    "url": "/static/js/main.4ec1d830.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a333dd01b6e4f5661c822416a3959626",
+    "url": "/static/media/Background.a333dd01.png"
+  }
+]);
